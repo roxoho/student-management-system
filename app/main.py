f0c255661f7a2520998420336e48395051a901f3
roxoho <as4372056@gmail.com>
2024-12-02 17:04:10 +0530

    main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 import uvicorn
 from app.routes import app
+import os
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=10000)
+    load_dotenv()
+    port = int(os.getenv("PORT", 10000))
+
+    uvicorn.run(app, host="0.0.0.0", port=port)
